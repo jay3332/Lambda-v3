461c8d18b5a481fe996a251e1bb8835ebb2c9f11
jay3332 <40323796+jay3332@users.noreply.github.com>
2023-07-23 20:52:53 -0400

    fix error

--- app/util/common.py
+++ app/util/common.py
@@ -197,11 +197,14 @@ def wrap_exceptions(exc_type: Type[BaseException]) -> Callable[[Callable[P, R]],
     return decorator
 
 
-class ExecutorFunction(Protocol['P', 'R']):
-    __sync__: Callable[P, R]
+if TYPE_CHECKING:
+    class ExecutorFunction(Protocol[P, R]):
+        __sync__: Callable[P, R]
 
-    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> Awaitable[R]:
-        ...
+        def __call__(self, *args: P.args, **kwargs: P.kwargs) -> Awaitable[R]:
+            ...
+else:
+    ExecutorFunction: Type[Callable] = Callable
 
 
 def executor_function(func: Callable[P, R]) -> ExecutorFunction[P, R]:
